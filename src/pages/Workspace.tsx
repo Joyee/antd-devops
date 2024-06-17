69cae3b47798fb8786706eb9c63c16076aa44a41
Joyee <veraqxf@gmail.com>
2024-06-17 18:05:50 +0800

    feat: 工作区

--- src/pages/Workspace.tsx
+++ src/pages/Workspace.tsx
@@ -1,8 +1,28 @@
-import { PageContainer } from '@ant-design/pro-components';
+import { PageContainer, GridContent } from '@ant-design/pro-components';
+import { Button } from 'antd';
 import React from 'react';
 
 const Workspace: React.FC = () => {
-  return <PageContainer>工作区</PageContainer>;
+  return (
+    <PageContainer
+      header={{
+        title: '工作区',
+        style: { backgroundColor: '#fff' },
+        extra: [
+          <Button key="1" type="primary">
+            创建应用
+          </Button>,
+        ],
+      }}
+    >
+      <GridContent>
+        <div>
+          <div className="main">Main</div>
+          <div className="right">Right</div>
+        </div>
+      </GridContent>
+    </PageContainer>
+  );
 };
 
 export default Workspace;
